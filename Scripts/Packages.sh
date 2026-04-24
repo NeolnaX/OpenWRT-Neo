ac422f13a585737b0781660c341bb5bf7a6f3baa
NeolnaX <122185836+NeolnaX@users.noreply.github.com>
2026-04-24 22:51:15 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -75,6 +75,8 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
+UPDATE_PACKAGE "UA-Mask" "Zesuy/UA-Mask" "main"
+UPDATE_PACKAGE "UA3F" "SunBK201/UA3F" "master"
 
 #更新软件包版本
 UPDATE_VERSION() {
